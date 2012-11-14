078931215faedd5aba736b92b9edf32150918590
Pankaj More <pankajmore@gmail.com>
2012-11-14 12:23:20 +0530

    removed duplicates in politics economics dataset

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -1,7 +1,7 @@
 import os
-folder="/home/pankajm/Downloads/datas/"
-folder1= folder+"Politics/non-controversial/"
-folder2= folder+"Economics/non-controversial/"
+folder="/home/pankajm/prog/Vivaad/data/Politics_ Economics/"
+folder1= folder+"controversial/"
+folder2= folder+"non-controversial/"
 
 common = set(os.listdir(folder1)).intersection(set(os.listdir(folder2)))
 
